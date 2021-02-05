91261f61f979efb7aa6bb7a961118771bcb4d53c
aox.lei <2387813033@qq.com>
2021-02-05 10:15:22 +0800

    1

--- manage.py
+++ manage.py
@@ -3,27 +3,31 @@ import json
 import sys
 from datetime import datetime, timedelta
 from pathlib import Path
-
 import akshare as ak
 import qdarkstyle
-from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer
-from PyQt5.QtWidgets import (QApplication, QCompleter, QHeaderView,
-                             QMainWindow, QTableWidget, QTableWidgetItem)
-from tinydb import Query, TinyDB
+from PySide2.QtCore import Qt, QThread, Signal, QTimer
+from PySide2.QtWidgets import (QApplication, QCompleter, QHeaderView,
+                               QMainWindow, QAbstractItemView, QItemDelegate)
+from PySide2.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel, QSqlQuery
 
 from main_window import Ui_MainWindow
 
 ROOT_PATH = Path.cwd()
 DATA_PATH = ROOT_PATH.joinpath('data.txt')
-db = TinyDB(ROOT_PATH.joinpath('db.json'))
-Query = Query()
+DB_PATH = ROOT_PATH.joinpath('database.db').as_posix()
+
+
+class EmptyDelegate(QItemDelegate):
+    def __init__(self, parent):
+        super(EmptyDelegate, self).__init__(parent)
+
+    def createEditor(self, QWidget, QStyleOptionViewItem, QModelIndex):
+        return None
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     search_completer = None
     # 表格列和索引对应关系, 顺序不能改
-    table_fund_column = ['code', 'name', 'type', 'yesterday_unit_value',
-                         'assess_unit_value', 'assess_enhance_rate', 'hold_cost', 'hold_money', 'assess_profit']
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -31,18 +35,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.resetUi()
 
         self.line_search.textChanged.connect(self.init_search)
-        self.init_data()
+        self.init_db()
         self.flush_assess_timer()
 
-        self.render_table()
-        self.table_fund.cellChanged.connect(self.update_table)
+    def init_db(self):
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName(DB_PATH)
+        self.db.open()
 
-    def update_table(self, row, column):
-        text = self.table_fund.item(row, column).text()
-        fund_code = self.table_fund.item(row, 0).text()
+        self.model = FundQSqlTableModel(
+            parent=self, db=self.db, table_view=self.table_fund)
 
-        key = self.table_fund_column[column]
-        db.update({key: text}, Query.code == fund_code)
+        self.table_fund.setModel(self.model)
+        self.table_fund.setColumnHidden(0, True)
 
     def init_search(self, value=''):
         if value == '':
@@ -62,42 +67,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.click_search_completer)
             self.line_search.setCompleter(self.search_completer)
 
-    def click_search_completer(self):
-        text = self.line_search.text()
+    def click_search_completer(self, text):
         text = text.split('|')
         data = {
             'code': text[0],
             'name': text[1],
             'type': text[2]
         }
-        self.replace_fund_data(data)
-
-    def render_table(self):
-        data = db.all()
-        if not data:
-            return True
-        row_count = len(data)
-
-        self.table_fund.setRowCount(row_count)
-        for index, info in enumerate(data):
-            for k, v in info.items():
-                if k in self.table_fund_column:
-                    _index = self.table_fund_column.index(k)
-                    item = QTableWidgetItem(v)
-                    if k not in ['hold_cost', 'hold_money']:
-                        item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
-                    self.table_fund.setItem(index, _index, item)
-
-    def replace_fund_data(self, data):
-        if db.search(Query.code == data.get('code')):
-            db.update(data, Query.code == data.get('code'))
-        else:
-            if 'hold_cost' not in data:
-                data['hold_cost'] = 0
-            if 'hold_money' not in data:
-                data['hold_money'] = 0
-            db.insert(data)
-        self.render_table()
+        self.model.flush_table(data)
 
     def init_data(self):
         self.thread = InitDataThread()
@@ -106,16 +83,63 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def flush_assess_timer(self):
         self.timer = QTimer(self)
-        self.timer.start(3000)
+        self.timer.start(2000)
         self.timer.timeout.connect(self.flush_assess)
 
     def flush_assess(self):
-        self.flush_assess_thread = FlushAssessThread()
-        self.flush_assess_thread.line_data.connect(self.replace_fund_data)
+        self.flush_assess_thread = FlushAssessThread(self)
+        self.flush_assess_thread.line_data.connect(self.update_assess)
         self.flush_assess_thread.start()
+
+    def update_assess(self, data):
+        match = self.model.match(self.model.index(
+            0, 1), Qt.DisplayRole, data.get('code'))
+        if match:
+            for k, v in data.items():
+                self.model.setData(self.model.index(
+                    match[0].row(), self.model.fieldIndex(k)), v)
 
     def resetUi(self):
         pass
+
+
+class FundQSqlTableModel(QSqlTableModel):
+    header_data = ['id', '基金代码', '基金名称', '基金类型', '昨日净值',
+                   '估算净值', '估算增长率', '持仓成本单价', '持仓金额', '预估收益']
+
+    def __init__(self, parent=None, db=None, table_view=None):
+        super(FundQSqlTableModel, self).__init__(parent, db)
+        self.table_view = table_view
+        self.setTable('fund')
+        self.setEditStrategy(QSqlTableModel.OnFieldChange)
+        for index, column in enumerate(self.header_data):
+            self.setHeaderData(index, Qt.Horizontal, column)
+        for i in [0, 1, 2, 3, 4, 5, 6, 9]:
+            self.table_view.setItemDelegateForColumn(
+                i, EmptyDelegate(self))
+
+        self.select()
+        self.beforeInsert.connect(self.before_insert)
+
+    def before_insert(self, record):
+        default_rule = ['yesterday_unit_value', 'assess_unit_value',
+                        'assess_enhance_rate', 'hold_cost', 'hold_money', 'assess_profit']
+        for field in default_rule:
+            if record.isNull(field):
+                record.setValue(field, 0)
+
+    def flush_table(self, data):
+        math = self.match(self.index(
+            0, 1), Qt.DisplayRole, data.get('code'))
+        if not math:
+            record = self.record()
+            for k, v in data.items():
+                record.setValue(k, v)
+            result = self.insertRecord(self.rowCount(), record)
+            if result:
+                self.select()
+            return False
+        return True
 
 
 class InitDataThread(QThread):
@@ -134,46 +158,63 @@ class InitDataThread(QThread):
 
 
 class FlushAssessThread(QThread):
-    line_data = pyqtSignal(dict)
+    line_data = Signal(dict)
 
     def __init__(self, parent=None):
+        self.parent = parent
         super(FlushAssessThread, self).__init__(parent)
 
     def run(self):
-        data = db.all()
-        if not data:
+        fund_list = {}
+        model = self.parent.model
+        for index in range(0, model.rowCount() + 1):
+            _hold_cost = model.record(index).value('hold_cost')
+            _hold_money = model.record(index).value('hold_money')
+            if not _hold_cost:
+                _hold_cost = 0
+            if not _hold_money:
+                _hold_money = 0
+            fund_list[model.record(index).value('code')] = {
+                'hold_cost': float(_hold_cost),
+                'hold_money': float(_hold_money)
+            }
+        if not fund_list:
             return True
-        fund_codes = [info.get('code') for info in data]
+        fund_codes = list(fund_list.keys())
         df = ak.fund_em_value_estimation()
         df = df[df['基金代码'].isin(fund_codes)]
         now = datetime.now().strftime('%Y-%m-%d')
         yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-
+        before_yesterday = (
+            datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')
         columns = {
             '基金代码': 'code',
             now + '-估算值': 'assess_unit_value',
             now + '-估算增长率': 'assess_enhance_rate',
-            yesterday + '-单位净值': 'yesterday_unit_value'
+            yesterday + '-单位净值': 'yesterday_unit_value',
+            yesterday + '-估算值': 'assess_unit_value',
+            yesterday + '-估算增长率': 'assess_enhance_rate',
+            before_yesterday + '-单位净值': 'yesterday_unit_value'
         }
         df.rename(columns=columns, inplace=True)
+
         df = df[['code', 'assess_unit_value',
                  'assess_enhance_rate', 'yesterday_unit_value']]
-        for index, row in df.iterrows():
-            row_data = row.to_dict()
-            fund_info = db.get(Query.code == row_data.get('code'))
-            hold_cost = fund_info.get('hold_cost', 0)
-            hold_money = fund_info.get('hold_money', 0)
-            if hold_cost == '':
-                hold_cost = 0
-            if hold_money == '':
-                hold_money = 0
+        for k, v in fund_list.items():
+            _df = df[df['code'] == k]
+            if len(_df) == 0:
+                continue
 
-            hold_cost = float(hold_cost)
-            hold_money = float(hold_money)
+            row = _df.iloc[-1]
+            row_data = row.to_dict()
+
+            hold_cost = float(v.get('hold_cost'))
+            hold_money = float(v.get('hold_money'))
+
             if hold_cost and hold_money:
                 share = float(format(hold_money / hold_cost, '0.2f'))
                 profit = float(format(
-                    share * (float(row_data['yesterday_unit_value']) - float(row_data['assess_unit_value'])), '0.2f'))
+                    share * (float(row_data['assess_unit_value']) - float(row_data['yesterday_unit_value'])), '0.2f'))
                 row_data['assess_profit'] = profit
 
             self.line_data.emit(row_data)
@@ -184,4 +225,4 @@ if __name__ == "__main__":
     app.setStyleSheet(qdarkstyle.load_stylesheet())
     main = MainWindow()
     main.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
