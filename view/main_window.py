b1683fe9ee621cb6c62b963352fd5e8c7cbb23ed
aox.lei <2387813033@qq.com>
2021-02-09 08:37:52 +0800

    1

--- view/main_window.py
+++ view/main_window.py
@@ -3,14 +3,19 @@
 ################################################################################
 ## Form generated from reading UI file 'mainwindow.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 5.14.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import *
-from PySide2.QtGui import *
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
+
+from searchlineedit import SearchLineEdit
 
 
 class Ui_MainWindow(object):
@@ -42,7 +47,7 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.btn_sync)
 
-        self.line_search = QLineEdit(self.centralwidget)
+        self.line_search = SearchLineEdit(self.centralwidget)
         self.line_search.setObjectName(u"line_search")
 
         self.horizontalLayout.addWidget(self.line_search)
